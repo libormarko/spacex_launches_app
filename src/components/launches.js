d7c8de5063bd47f4956fa4a587be05dc3ca39cb6
Libor Marko <libor.marko@accenture.com>
2021-09-02 09:17:21 +0200

    css bug fixed and no results info message added

--- src/components/launches.js
+++ src/components/launches.js
@@ -4,18 +4,20 @@ import PropTypes from 'prop-types';
 const Launches = ({ results }) => (
     <div className="launches__container">
         {
-            results && results.map(result => 
-                <Launch
-                    key={`${result.mission_name}-${result.launch_date_unix.toString()}`.replace(/\s+/g, '')}
-                    launchDateUnix={result.launch_date_unix}
-                    missionPatch={result.links.mission_patch_small}
-                    missionName={result.mission_name}
-                    missionId={result.mission_id}
-                    launchYear={result.launch_year} 
-                    launchSuccess={result.launch_success}
-                    landSuccess={result.rocket.first_stage.cores[0].land_success}
-                />
-            )
+            results && results.length === 0 
+                ?   <div className="no-results-status">There are no launches for your criteria...</div>
+                :   results && results.map(result => 
+                        <Launch
+                            key={`${result.mission_name}-${result.launch_date_unix.toString()}`.replace(/\s+/g, '')}
+                            launchDateUnix={result.launch_date_unix}
+                            missionPatch={result.links.mission_patch_small}
+                            missionName={result.mission_name}
+                            missionId={result.mission_id}
+                            launchYear={result.launch_year} 
+                            launchSuccess={result.launch_success}
+                            landSuccess={result.rocket.first_stage.cores[0].land_success}
+                        />
+                )
           }
     </div>
 );
