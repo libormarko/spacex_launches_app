7de1e34582b0003e6f9fd814d784eca757e2be3e
Libor Marko <libor.marko@accenture.com>
2021-09-14 09:26:01 +0200

    launch landing success API read logic improved

--- src/components/launch.js
+++ src/components/launch.js
@@ -16,11 +16,11 @@ const LaunchCard = ({ missionPatch, missionName, missionId, launchYear, launchSu
         </div>
         <div className="item__container">
             <h6>Succesful Launch:</h6>
-            <span>{launchSuccess ? launchSuccess.toString() : "false"}</span>
+            <span>{launchSuccess === null ? "unclear data" : launchSuccess.toString()}</span>
         </div>
         <div className="item__container">
             <h6>Succesful Landing:</h6>
-            <span>{landSuccess === null ? "false" : landSuccess.toString()}</span>
+            <span>{landSuccess === null ? "no intent to land" : landSuccess.toString()}</span>
         </div>
     </div>
 );
